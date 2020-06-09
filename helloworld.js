f252c4244fb587eafceef71b8bec0286bfa1f856
AlejandroTCX <alex.6161@hotmail.com>
2020-06-09 15:48:37 -0500

    First exercise

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,12 @@
-console.log('From branch day1');
+//comments can make code readable
+//Bienvenido a 30 Días de JavaScript
+/* comments can make code readable
+    easy to reuse and informative */
+let variable1="Empezando con un carácter";
+let variable2=Boolean;
+let variable3;
+let variable4=null;
+console.log(typeof variable1);
+console.log(typeof variable2);
+console.log(typeof variable3);
+console.log(typeof variable4);
